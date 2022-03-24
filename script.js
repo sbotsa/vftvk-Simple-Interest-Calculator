14ae721c3f2d1ba0c609cbd1675b4f03797f8f13
sbotsa <102288330+sbotsa@users.noreply.github.com>
2022-03-24 15:16:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function cal()
   var p = document.getElementById("pri").value;
   var r = document.getElementById("rate").value;
   var t = document.getElementById("years").value;
-  var si=(p*t*r)/100;
+  var si=(p*t*12*r)/100;
   document.getElementById("result").innerHTML=""+si;
 
 }
